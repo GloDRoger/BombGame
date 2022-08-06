3dc60e6acb03630ed625f3c496a89b2533832000
GloDRoger <414946261@qq.com>
2022-08-06 21:58:15 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base:'BombGame',
+  plugins: [vue()],
+  build:{
+    outDir:'docs',
+  }
 })
